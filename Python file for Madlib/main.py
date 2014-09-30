9fdbde761c5f55bb7653af24b0abeda66da544f3
Cameron Kozinski <camski20@gmail.com>
2014-09-30 12:55:17 -0400

    Added a "alert" to let the user know info will be requested, and added a raw_input asking for the users name

---
+++ Python file for Madlib/main.py
@@ -0,0 +1,7 @@
+#alert the user that I will be asking questions
+print "Hello, I just need some info to create your Mad Lib. Please answer all of the questions."
+
+user_name = raw_input("Please enter your name.  ")
+
+print user_name
+
