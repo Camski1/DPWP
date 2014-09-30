f07de86ee3f032cbc0be727bbf5b3665e07c4653
Cameron Kozinski <camski20@gmail.com>
2014-09-30 14:24:34 -0400

    Finished adding to the required 3 str's and 3 int's

--- Python file for Madlib/main.py
+++ Python file for Madlib/main.py
@@ -1,7 +1,11 @@
 #alert the user that I will be asking questions
 print "Hello, I just need some info to create your Mad Lib. Please answer all of the questions."
-
-user_name = raw_input("Please enter your name.  ")
-
+#info that will be taken from the user
+user_name = raw_input("Please enter your name.  ")# str
+user_age = raw_input("Please enter your age.  ")# int
+user_book = raw_input("Please enter the name of your favorite book.  ")#str
+user_fav_num = raw_input("Please enter your lucky number.  ")# int
+user_bike = raw_input("Please enter the age you learned to ride a bike.  ")# int
+user_sex = raw_input("Are you a boy or a girl?  ")#str
 print user_name
 
