45b6b456f46e34b3c863ed94b85fc8b3b74e87d6
Cameron Kozinski <camski20@gmail.com>
2014-10-07 21:46:07 -0400

    Properly connected main.py to pages.py so that information can run freely. Also, added some test code to be changed to fit assignment.

--- Lab 2 Server Side Form/simple-form/main.py
+++ Lab 2 Server Side Form/simple-form/main.py
@@ -5,11 +5,20 @@ Design Patterns for Web Programming
 simple form
 '''
 import webapp2 #use webapp2 library
-from pages import Page #importing Page class from pages.py
+from pages import Page#importing Page class from pages.py
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(Page().print_page())
+        p = Page()
+        if self.request.GET:
+            user_name = self.request.GET['user_name']
+            user_email = self.request.GET['user_email']
+            all = p.page_head + p.page_form + p.page_review + p.page_close
+            all = all.format(**locals())
+            self.response.write(all) #print info to page
+        else:
+            self.response.write(p.page_head + p.page_form + p.page_close) #print info to page
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- Lab 2 Server Side Form/simple-form/pages.py
+++ Lab 2 Server Side Form/simple-form/pages.py
@@ -9,14 +9,29 @@ class Page(object):
     </head>
     <body>
         '''
-        self.page_div = '''
-        <div>Hello</div>'''
+        self.page_form = '''
+        <div>
+            <form method="GET">
+            <label>Full Name:</label><br>
+            <input type="text" name="user_name" /><br>
+
+            <label>Email:</label><br>
+            <input type="email" name="user_email" /><br>
+
+            <input type="submit" value="Submit" />
+            </form>
+        </div>'''
+        self.page_review = '''
+        <div>
+            <p>{user_name}</p>
+        </div>'''
         self.page_close = '''
     </body>
 </html>
         '''
 
-    def print_page(self):
-        all = self.page_head + self.page_div + self.page_close
-        all = all.format(**locals())
-        return all
+
+        def print_out(self):
+            all = self.page_head + self.page_form + self.page_review + self.page_close
+            all = all.format(**locals())
+            return all
