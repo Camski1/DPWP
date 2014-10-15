fca81aeac8e4b7b2afb2113d98c45a2aa8c50beb
Cameron Kozinski <camski20@gmail.com>
2014-10-14 21:36:36 -0400

    Added some of the contents to the MovieData class

--- reusable-libraries/lib.py
+++ reusable-libraries/lib.py
@@ -1,1 +1,14 @@
 class MovieData(object):
+    def __init__(self):
+        self.title = ""
+        self.__year = 0
+        self.director = ""
+
+    @property
+    def year(self):
+        pass
+    @year.setter
+    def year(self, y):
+        if y > 2014:
+            
+        self.__year = y
--- reusable-libraries/main.py
+++ reusable-libraries/main.py
@@ -1,12 +1,16 @@
 
 import webapp2
-
+from lib import MovieData
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         #movie
         #year
         #director
-        pass
+        md1 = MovieData()
+        md1.title = "The Princess Bride"
+        md1.director = "Rob Reiner"
+        md1.year = 1989
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
