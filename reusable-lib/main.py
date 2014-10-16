20fbc12007af66467985a8eff2444a8a3415a686
Cameron Kozinski <camski20@gmail.com>
2014-10-16 00:36:09 -0400

    Made my form class and added the questions for the user. Also, added the results page class and filled it with the html ground work.

--- reusable-lib/main.py
+++ reusable-lib/main.py
@@ -5,10 +5,13 @@ Design Patterns for Web Programming
 Reusable Library
 '''
 import webapp2
+from pages import ResultsPage, FormPage
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        rp = ResultsPage()
+        fp = FormPage()
+        self.response.write(fp.form_page_head + fp.page_form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
---
+++ reusable-lib/pages.py
@@ -0,0 +1,60 @@
+class ResultsPage(object):
+    def __init__(self):
+        #The page_head var is holding only static information
+        self.page_head = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>You're using coconuts!</title>
+        <link href="css/main.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        hello
+    </body>
+</html>
+        '''
+
+
+class FormPage(object):
+    def __init__(self):
+        #The page_head var is holding only static information
+        self.form_page_head = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Ask me the questions</title>
+        <link href="css/main.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <h1>Ask me the questions, bridgekeeper. I am not afraid</h1>
+        '''
+        self.page_form = '''
+        <div class="my_form">
+            <form method="GET">
+                <label for="name">What... is your name?</label><br>
+                <input class="inp_name" type="text" name="user_name" id="name"/><br>
+
+                <label for="quest">What... is your quest?</label><br>
+                <textarea id="quest" name="user_quest" rows="7" cols="30"></textarea><br>
+
+                <label for="color">What... is your favorite color?</label><br>
+                <input id="color" class="inp_color" type="text" name="user_color" /><br>
+
+                <label for="location">Where are you from?</label><br>
+                <input id="location" type="text" name="user_location" /><br>
+
+                <input id="ma-sex" type="radio" name="sex" value="male" checked>
+				<label for="ma-sex">Male</label>
+				<input id="fe-sex" type="radio" name="sex" value="female">
+				<label for="fe-sex">Female</label><br>
+
+				<label for="test">What... is F!VE plus F@UR? (just want to see if you are human)</label><br>
+                <input id="test" type="text" name="user_test" /><br>
+
+                <input class="sub_btn" type="submit" value="Submit" />
+            </form>
+        </div>
+
+    </body>
+</html>
+        '''
