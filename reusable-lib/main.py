8624606b96b37b38661a603a63eaa8d166877d0b
Cameron Kozinski <camski20@gmail.com>
2014-10-16 00:56:38 -0400

    Added the if else statements to find if user info has been added and added all my GET's to get the information

--- reusable-lib/main.py
+++ reusable-lib/main.py
@@ -11,7 +11,19 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         rp = ResultsPage()
         fp = FormPage()
-        self.response.write(fp.form_page_head + fp.page_form)
+        if self.request.GET:
+            user_name = self.request.GET['user_name']
+            user_quest = self.request.GET['user_quest']
+            user_color = self.request.GET['user_color']
+            user_location = self.request.GET['user_location']
+            sex = self.request.GET['sex']
+            user_test = self.request.GET['user_test']
+            all = rp.page_head
+            all = all.format(**locals())
+            self.response.write(all)
+        else:
+            self.response.write(fp.form_page_head + fp.page_form)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
