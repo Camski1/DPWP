a289cf528c1ea96d7c6a0fa5344fb89a455c8cbc
Cameron Kozinski <camski20@gmail.com>
2014-10-21 20:14:31 -0400

    added all of the recipes to data.py

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -1,6 +1,6 @@
 class RecData(object):
     def __init__(self):
-        self.rec_arr = [[],[],[]]
+        self.rec_arr = [[],[],[],[],[]]
 
 class Bigos(RecData):
     def __init__(self):
@@ -20,7 +20,7 @@ class PotatoCakes(RecData):
     def __init__(self):
         RecData.__init__(self)
         self.rec_name = "Placki Ziemniaczane"
-        self.rec_desc = "lacki ziemniaczane is a Polish name for a quite well-known, simple and good food made from grated potatoes fried in a fat."
+        self.rec_desc = "Placki ziemniaczane is a Polish name for a quite well-known, simple and good food made from grated potatoes fried in a fat."
         self.rec_ingr = ["4-5 large potatoes, peeled and grated", "1 large onion", "3-4 minced garlic cloves", "2 eggs", "4 tbsp flour ", "1/2 teaspoon marjoram", "1/2 teaspoon salt (minimum)", "Freshly ground black pepper"]
         self.directions = ["Using a food processor or hand shredder, grate potatoes and onion using the finest grader.  The finer the potatoes are grated, the better because it is easier to cook.  If you grate the potatoes in larger chunks, you will just have to cook longer and use a little bit more oil for taste.", "Squeeze potatoes and onions with hands to remove excess liquid", "Put grated potatoes and onion in a large bowl and mix with eggs, garlic, flour, marjoram, salt, and pepper.", "Heat oil in large frying pan until almost smoking.  Carefully put a large spoonful of the potato mixture into the pan and flatten with a fork.  Repeat until you have 3-4 pancakes in the pan. Fry each pancake until it is golden brown on both sides.  Remove with spatula and drain on a paper towel.", "Serve potato pancakes warm with topping of choice."]
 
@@ -31,8 +31,30 @@ class Zrazy(RecData):
     def __init__(self):
         RecData.__init__(self)
         self.rec_name = "Zrazy"
-        self.rec_desc = "lacki ziemniaczane is a Polish name for a quite well-known, simple and good food made from grated potatoes fried in a fat."
-        self.rec_ingr = ["4-5 large potatoes, peeled and grated", "1 large onion", "3-4 minced garlic cloves", "2 eggs", "4 tbsp flour ", "1/2 teaspoon marjoram", "1/2 teaspoon salt (minimum)", "Freshly ground black pepper"]
-        self.directions = ["Using a food processor or hand shredder, grate potatoes and onion using the finest grader.  The finer the potatoes are grated, the better because it is easier to cook.  If you grate the potatoes in larger chunks, you will just have to cook longer and use a little bit more oil for taste.", "Squeeze potatoes and onions with hands to remove excess liquid", "Put grated potatoes and onion in a large bowl and mix with eggs, garlic, flour, marjoram, salt, and pepper.", "Heat oil in large frying pan until almost smoking.  Carefully put a large spoonful of the potato mixture into the pan and flatten with a fork.  Repeat until you have 3-4 pancakes in the pan. Fry each pancake until it is golden brown on both sides.  Remove with spatula and drain on a paper towel.", "Serve potato pancakes warm with topping of choice."]
+        self.rec_desc = "Zrazy is an traditional Old Polish food coming from the Polish Gentry and the cuisine of hunters. Zrazy is made from slices of beef, veal or game, stir-fried and stewed with an addition of vegetables and spices."
+        self.rec_ingr = ["700 g beef", "2 tbsp grainy mustard", "200 g smoked bacon, cut into strips", "200 g gherkin, cut into thin strips", "200 g brown onion, cut into strips", "500 ml beef stock"]
+        self.directions = ["Cut the beef into 4 to 6 pieces and pound until thin. Spread each slice of beef with the mustard. On each slice, place a piece of bacon, gherkin and some onion. Roll up the beef slices and secure with toothpicks.","Heat oil in a deep frying pan and fry the beef roulades until brown on each side. Add 1/2 cup of beef stock. Simmer for 1 hour or until cooked, adding more stock when necessary. Season to taste.", "Serve with mashed beetroot."]
+
+        self.rec_array = [self.rec_name, self.rec_desc, self.rec_ingr, self.directions]
+
+
+class Nalesniki(RecData):
+    def __init__(self):
+        RecData.__init__(self)
+        self.rec_name = "Nalesniki"
+        self.rec_desc = "Typical of many cuisines, Polish nalesniki are just pancakes with various fillings (from sweet to spicy). Nalesniki evolved from Old Polish pies and pancakes under the influence of French cuisine, and have been very popular in Poland for decades."
+        self.rec_ingr = ["1/2 cup all-purpose flour", "1/2 cup milk", "1/4 cup lukewarm water", "2 large eggs", "2 tablespoons butter, melted", "1/2 teaspoon salt"]
+        self.directions = ["In a blender or food processor, combine all ingredients until smooth. Transfer to a pitcher, cover with plastic wrap and let rest for 30 minutes so the liquid can be absorbed by the flour.","Using a 2-ounce ladle, portion out batter into a nonstick crepe pan or small skillet that has been lightly coated with butter. Rotate pan and swirl batter until it covers the entire bottom of pan. Cook until lightly brown or spotted brown on the underside. Turn and cook second side until light brown.", "Remove to waxed paper or parchment paper and repeat with remaining batter and butter. Serve immediately or wrap and freeze up to 1 month."]
+
+        self.rec_array = [self.rec_name, self.rec_desc, self.rec_ingr, self.directions]
+
+
+class Golumpki(RecData):
+    def __init__(self):
+        RecData.__init__(self)
+        self.rec_name = "Golumpki"
+        self.rec_desc = "Golabki or stuffed cabbage is one of a traditional food of Central and Eastern Europe. Polish golabki is a cooked knob of forcemeat wrapped up in a leaf of a white cabbage."
+        self.rec_ingr = ["2 c. cooked rice", "1 to 1 1/2 lbs. raw ground beef", "1/4 tsp. salt & pepper", "1 med. green head of cabbage"]
+        self.directions = ["Mix together the rice, salt/pepper, and raw ground beef. Boil the head of cabbage after coring it, just to loosen the leaves for about 3 to 5 minutes. Take 1 leaf out at a time and fill it with approximately 2 tablespoonfuls of rice-meat mixture. Fold it over horizontally and then bring in the sides of leaf to make a pocket. Arrange in a covered casserole dish or roasting pan and bake at 350 degrees for 1 hour."]
 
         self.rec_array = [self.rec_name, self.rec_desc, self.rec_ingr, self.directions]
--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -1,7 +1,7 @@
 
 import webapp2
 from pages import Page
-from data import RecData, Bigos, PotatoCakes, Zrazy
+from data import RecData, Bigos, PotatoCakes, Zrazy, Nalesniki, Golumpki
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -10,9 +10,13 @@ class MainHandler(webapp2.RequestHandler):
         bigos = Bigos()
         pc = PotatoCakes()
         zr = Zrazy()
+        nal = Nalesniki()
+        gol = Golumpki()
         rd.rec_arr[0] = bigos.rec_array
         rd.rec_arr[1] =pc.rec_array
         rd.rec_arr[2] =zr.rec_array
+        rd.rec_arr[3] =nal.rec_array
+        rd.rec_arr[4] =gol.rec_array
         for item in rd.rec_arr:
             print item[0]
         self.response.write(p.print_page())
